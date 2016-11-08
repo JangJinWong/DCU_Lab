3157c74419399234501d9a77f40e227a3730d01e
Sukwon Choi <swchoi06@wafflestudio.com>
2016-11-09 01:00:12 +0900

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+BASEDIR=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
+BASHRC="${HOME}/.bashrc"
+
+# Set Gazebo Path
+PLUGIN_PATH="${BASEDIR}Firmware/Tools/sitl_gazebo/Build"
+if !(echo "$GAZEBO_PLUGIN_PATH" | grep -q "${PLUGIN_PATH}"); then
+  echo "" >> $BASHRC""
+  echo "# Set Gazebo Plugin Path" >> "$BASHRC"
+  echo "export GAZEBO_PLUGIN_PATH=\${GAZEBO_PLUGIN_PATH}:${PLUGIN_PATH}" >> "$BASHRC"
+fi
+
+MODELS_PATH="${BASEDIR}Firmware/Tools/sitl_gazebo/models"
+CUSTOM_MODELS_PATH="${BASEDIR}gazebo_models"
+if !(echo "$GAZEBO_MODEL_PATH" | grep -q "${MODELS_PATH}"); then
+  echo "# Set Gazebo Model Path" >> "$BASHRC"
+  echo "export GAZEBO_MODEL_PATH=\${GAZEBO_MODEL_PATH}:${MODELS_PATH}" >> "$BASHRC"
+  if !(echo "$GAZEBO_MODEL_PATH" | grep -q "${CUSTOM_MODELS_PATH}"); then
+    echo "export GAZEBO_MODEL_PATH=\${GAZEBO_MODEL_PATH}:${CUSTOM_MODELS_PATH}" >> "$BASHRC"
+  fi
+fi
